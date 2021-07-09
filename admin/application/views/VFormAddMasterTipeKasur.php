838218c504116df44249b415fca6e346004cf770
Ferdian Nur <68576009+ferdiannur11@users.noreply.github.com>
2021-07-09 13:46:24 +0700

    push

---
+++ admin/application/views/VFormAddMasterTipeKasur.php
@@ -0,0 +1,53 @@
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+     <!-- Content Header (Page header) -->
+     <div class="content-header">
+         <div class="container-fluid">
+             <div class="row mb-2">
+                 <div class="col-sm-6">
+                     <h1 class="m-0 text-dark">Tambah Master Tipe Kasur</h1>
+                 </div>
+                 <!-- <div class="col-sm-6">
+                     <ol class="breadcrumb float-sm-right">
+                         <li class="breadcrumb-item"><a href="#">Home</a></li>
+                         <li class="breadcrumb-item active">Dashboard v1</li>
+                     </ol>
+                 </div>/.col -->
+             </div><!-- /.row -->
+         </div><!-- /.container-fluid -->
+     </div>
+     <!-- /.content-header -->
+
+     <!-- Main content -->
+     <section class="content">
+         <div class="container-fluid">
+             <!-- Small boxes (Stat box) -->
+             <div class="box">
+                 <div class="box-header with-border">
+                     <div class="row">
+                         <div class="col-12">
+                             <form action="<?php echo site_url('Master_tipe_kasur/AddDataMasterTipeKasur'); ?>" method="post" enctype="multipart/form-data">
+                                 <div class="card-body">
+
+                                     <div class="form-group">
+                                         <label>Nama Tipe Kasur</label>
+                                         <input name="nama_tipe_kasur" type="text" class="form-control" placeholder="Nama Tipe kasur">
+                                     </div>
+                                     <div class="box-body pad">
+                                         <label>Deskripsi</label>
+                                         <textarea class="textarea" name="deskripsi" placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
+
+									</textarea>
+
+                                     </div>
+
+                                     <div class="form-group">
+                                         <button type="submit" class="btn btn-primary">Submit</button>
+                                     </div>
+                                 </div>
+                             </form>
+                         </div>
+                     </div>
+                 </div>
+     </section>
+ </div>
