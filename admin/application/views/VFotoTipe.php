1a2229bbb5afa26a27c36207fad13c949f664159
Ferdian Nur <68576009+ferdiannur11@users.noreply.github.com>
2021-07-29 14:08:06 +0700

    push foto tipe CRUD

---
+++ admin/application/views/VFotoTipe.php
@@ -0,0 +1,104 @@
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+     <!-- Content Header (Page header) -->
+     <div class="content-header">
+         <div class="container-fluid">
+             <div class="col-sm-6">
+                 <h1 class="m-0 text-dark">Data Foto Tipe</h1>
+             </div>
+         </div><!-- /.container-fluid -->
+     </div>
+     <!-- /.content-header -->
+
+     <!-- Main content -->
+     <section class="content">
+         <div class="container-fluid">
+             <!-- Small boxes (Stat box) -->
+             <div class="box">
+                 <div class="box-header with-border">
+                     <div class="row">
+                         <div class="col-12">
+                             <div class="card">
+                                 <div class="card-header">
+                                     <a href="<?php echo site_url('Foto_tipe/VFormAddFotoTipe'); ?>" class="btn btn-success">
+                                         Tambah Data
+                                     </a>
+                                 </div>
+                                 <!-- /.card-header -->
+                                 <div class="card-body table-responsive p-3" style="height: 100%;">
+                                     <!-- <table class="table table-head-fixed text-nowrap"> -->
+                                     <table id="example1" class="table table-bordered table-striped">
+                                         <thead>
+                                             <tr>
+                                                 <th>No</th>
+                                                 <!-- <th>Kode Pegawai</th> -->
+                                                 <th>Nama Tipe Foto</th>
+                                                 <th>Flag Foto</th>
+                                                 <th>Tools</th>
+                                                 <!-- <th>
+ 													&nbsp;
+ 												</th> -->
+                                             </tr>
+                                         </thead>
+                                         <tbody>
+                                             <?php
+                                                if (!empty($FotoTipe)) {
+                                                    foreach ($FotoTipe as $index => $ReadDS) {
+                                                        $index = $index + 1;
+                                                ?>
+                                                     <tr>
+                                                         <td><?php echo $index ?></td>
+                                                         <td><?php echo $ReadDS->nama_tipe_foto; ?></td>
+                                                         <td>
+                                                             <?php if ($ReadDS->flag_foto == "1") {
+                                                                    echo 'Foto Properti';
+                                                                } else {
+                                                                    echo 'Foto Kamar';
+                                                                } ?></td>
+                                                         <td>
+                                                             <a href="<?php echo site_url('Foto_tipe/DataFotoTipe/' . $ReadDS->id . '/view'); ?>" class="btn btn-xs btn-info">
+                                                                 Edit
+                                                             </a>
+                                                             <a href="<?php echo site_url('Foto_tipe/DeleteDataFotoTipe/' . $ReadDS->id); ?>" class="btn btn-xs btn-danger">
+                                                                 Delete
+                                                             </a>
+
+                                                         </td>
+                                                     </tr>
+                                             <?php
+                                                    }
+                                                } ?>
+
+                                         </tbody>
+                                     </table>
+                                 </div>
+                                 <!-- /.card-body -->
+                             </div>
+                             <!-- /.card -->
+                         </div>
+                     </div>
+                 </div>
+     </section>
+ </div>
+ <script>
+     window.onload = function() {
+         /** Your code here. **/
+
+         if (localStorage.getItem("user_id") != null) {
+             var userID = document.getElementsByName("user_id");
+             var username = document.getElementsByName("username");
+             for (var x = 0; x < userID.length; x++) // comparison should be "<" not "<="
+             {
+                 userID[x].value = localStorage.getItem("user_id");
+             }
+
+             for (var x = 0; x < username.length; x++) // comparison should be "<" not "<="
+             {
+                 username[x].value = localStorage.getItem("username");
+             }
+
+         }
+
+
+     }
+ </script>
